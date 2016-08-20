ba5f60f1104c21a831d28bc2e29d97b895f6175f
Rishi Khaneja <rishikhaneja@gmail.com>
2016-08-20 19:19:13 +0800

    code clean up

--- mockingbird.py
+++ mockingbird.py
@@ -4,32 +4,39 @@ import glob
 import os
 import CppHeaderParser
 
-os.chdir('.')
-
 out_file = 'output.hpp'
 result = ''
 
-for file_name in glob.glob('*.hpp'):
-    if file_name != out_file:
+os.chdir('.')
+
+for file_name in (f for f in glob.glob('*.hpp') if f != out_file):
+
         try:
             cppHeader = CppHeaderParser.CppHeader(file_name)
+
             for class_name, class_items in cppHeader.classes.items():
+
                 result += 'struct Mock'+class_name+' : '+class_name+' {\n'
-                for public_method in class_items['methods']['public']:
-                    if not public_method['constructor'] and not public_method['destructor']:
+
+                for public_method in (m for m in class_items['methods']['public'] if not m['constructor'] and not m['destructor']):
+
                         result += '    MOCK_<const>METHOD_<arg_count>(<name>, <return>(<args>));\n'.\
                             replace('<const>', 'CONST_' if public_method['const'] else '').\
                             replace('<arg_count>', str(len(public_method['parameters']))).\
                             replace('<name>', public_method['name']).\
                             replace('<return>', public_method['returns']).\
                             replace('<args>', ', '.join('%s' % (parameter['type']) for parameter in public_method['parameters']))
+
                 result += '};\n'
+
         except CppHeaderParser.CppParseError as e:
             print(e)
             sys.exit(1)
+
 mock_file = open(out_file, 'w')
 mock_file.writelines(result)
 mock_file.close()
+
 print 'generating:'
 print result
 print 'success! see output.hpp'
